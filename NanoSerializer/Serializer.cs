832ed69a8ffbfc3957edcaf40cfba19d0e7e60b2
Sergey Seletsky <SeletskySergey@live.com>
2017-03-17 14:18:00 +0200

    cleanup

--- NanoSerializer/Serializer.cs
+++ NanoSerializer/Serializer.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Runtime.Serialization;
 using System.Reflection;
-using System.Threading;
 using NanoSerializer.Mappers;
 
 namespace NanoSerializer
