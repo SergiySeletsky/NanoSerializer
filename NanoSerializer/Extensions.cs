27d44d8447b64ba80e904d4c932ca82a8d37a912
Sergey Seletsky <SeletskySergey@live.com>
2020-03-03 22:50:57 +0200

    fixed

--- NanoSerializer/Extensions.cs
+++ NanoSerializer/Extensions.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace NanoSerializer
 {
     internal static class Extensions
     {
-        internal static short ReadLength(this ref NanoReader reader)
-        {
-            var span = reader.Buffer.Slice(reader.Position, sizeof(short));
-            reader.Position += sizeof(short);
+        const int LengthSize = sizeof(ushort);
 
-            return BitConverter.ToInt16(span);
+        internal static ushort ReadLength(this ref NanoReader reader)
+        {
+            var span = reader.Buffer.Slice(reader.Position, LengthSize);
+            reader.Position += LengthSize;
+
+            return BitConverter.ToUInt16(span);
         }
 
         internal static ReadOnlySpan<byte> Read(this ref NanoReader reader, int size)
